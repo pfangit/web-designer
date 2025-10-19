3a4930d2f8aad35fc80ea52e80d98e0a96da5124
pfangit <fanping2024@gmail.com>
2025-10-19 17:02:18 +0800

    feat(sketch): 实现拖拽组件到画布功能
    - 新增拖拽事件处理逻辑，支持从组件面板拖拽组件到画布
    - 添加事件发射器用于组件间通信
    - 更新组件图标宽度以优化显示效果
    - 配置 ESLint 和 Prettier代码风格检查
    - 引入 eventemitter3 库管理事件
    - 调整 widget-container 样式并启用垂直滚动条
    - 在 package.json 中新增 lint-staged 配置以自动格式化代码

--- src/container/sketch-container.tsx
+++ src/container/sketch-container.tsx
@@ -1,5 +1,34 @@
+import type { DragEvent } from 'react';
+import emitter from '../events/emitter.ts';
+import type { WidgetItem } from '../widgets/widget-items.tsx';
+
 const SketchContainer = () => {
-  return <div className="flex-1 bg-gray-100">画布</div>;
+  const onDrop = (e: DragEvent) => {
+    e.preventDefault();
+    if (e.dataTransfer) {
+      const item = JSON.parse(e.dataTransfer.getData('component')) as WidgetItem;
+      console.log(item);
+      emitter.emit('start-end', item);
+    }
+  };
+
+  emitter.on('drag-start', (item: WidgetItem) => {
+    console.log(item);
+  });
+
+  return (
+    <div
+      onDrop={onDrop}
+      onDragOver={(e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+      className="flex-1 bg-gray-100"
+    >
+      <div className="">拖拽位置</div>
+      <div>画布</div>
+    </div>
+  );
 };
 
 export default SketchContainer;
--- src/container/widget-collapse.tsx
+++ src/container/widget-collapse.tsx
@@ -1,6 +1,7 @@
 import { Collapse, type CollapseProps } from 'antd';
 import { type WidgetItem, widgetItems } from '../widgets/widget-items.tsx';
-import { type ReactNode } from 'react';
+import { type DragEvent, type ReactNode } from 'react';
+import emitter from '../events/emitter.ts';
 
 const WidgetCollapse = () => {
   const renderWidget = (widgets?: WidgetItem[]): ReactNode => {
@@ -22,13 +23,21 @@ const WidgetCollapse = () => {
             }
           ]);
 
+    const handleDragStart = (e: DragEvent, item: WidgetItem) => {
+      e.dataTransfer.setData('component', JSON.stringify(item));
+      console.log('drag-start', item);
+      emitter.emit('start-drag', item);
+    };
+
     return (
       <div className="gap-[1px] grid grid-cols-3 bg-[#d9d9d9]">
         {padWidgets.map((item) => {
           return (
             <div
+              draggable={true}
               key={item.type}
               style={item.style}
+              onDragStart={(e) => handleDragStart(e, item)}
               className={`flex-col ${item.name ? 'cursor-grab hover:text-blue-400 hover:shadow-[0_0_10px_rgba(0,0,0,0.1)]' : ''} flex items-center justify-center bg-white p-1 text-gray-600 `}
             >
               <div className="pb-2 pt-2">{item.icon}</div>
